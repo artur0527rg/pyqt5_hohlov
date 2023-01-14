cb9aabdd21100409c09dff2c489d6e62843617e1
ARTUR <rogozinskiyartur@gmail,com>
2023-01-14 15:24:46 +0300

    Знакомство Урок 3 CheckBox RadioButton

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sys
 import time
+import random
 
 from PyQt5 import QtCore, QtGui, QtWidgets
 
@@ -10,24 +11,43 @@ class MyWin(QtWidgets.QMainWindow):
         QtWidgets.QWidget.__init__(self, parent)
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
-        self.ui.pushButton.clicked.connect(self.handler)
-        self.ui.pushButton_2.clicked.connect(self.setcolor)
-         
-    def handler(self):
-        self.ui.pushButton.setDisabled(True)
-        self.ui.plainTextEdit.appendPlainText('text')
-        self.ui.label.setText('text')
+        self.ui.pushButton.clicked.connect(self.gettext)
+        self.ui.checkBox.clicked.connect(self.output)
 
-    def setcolor(self):
-        self.ui.pushButton.setText('11111')
-        self.ui.pushButton.setStyleSheet('background-color: red; color: white;')
-        self.ui.pushButton.setDisabled(False)
-        
+    def output(self):
+        name = self.ui.checkBox.text()
+        self.ui.radioButton.setText(str(random.randint(0, 100)))
+        print(name)
+
+    def gettext(self):
+        if self.ui.checkBox.isChecked():
+            checkbox_1 = self.ui.checkBox.text()
+        else:
+            checkbox_1 = ''
+
+        if self.ui.checkBox_2.isChecked():
+            checkbox_2 = self.ui.checkBox_2.text()
+        else:
+            checkbox_2 = ''
+
+        if self.ui.radioButton.isChecked():
+            radiobutton_1 = self.ui.radioButton.text()
+        else:
+            radiobutton_1 = ''
+
+        if self.ui.radioButton_2.isChecked():
+            radiobutton_2 = self.ui.radioButton_2.text()
+        else:
+            radiobutton_2 = ''
+
+        result = f'{checkbox_1}{checkbox_2}{radiobutton_1}{radiobutton_2} Hello world'
+
+        self.ui.plainTextEdit.appendPlainText(result)      
         
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
-    myapp =MyWin()
+    myapp = MyWin()
     myapp.show()
     sys.exit(app.exec_())
     
