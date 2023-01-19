c19434676fb22b1e82b82ea6b3d9cb7851ab2883
ARTUR <rogozinskiyartur@gmail,com>
2023-01-19 13:52:46 +0300

    Уведомления Урок 6 модальные окна через код, через qt designer.

--- main.py
+++ main.py
@@ -3,70 +3,47 @@ import sys
 from PyQt5 import QtCore, QtGui, QtWidgets
 
 from ui import *
-
+from mod import * 
+        
 
 class GUI(QtWidgets.QMainWindow):
     def __init__(self, parent=None):
         QtWidgets.QWidget.__init__(self, parent)
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
-        self.ui.pushButton.clicked.connect(self.notification)
-        self.ui.pushButton_2.clicked.connect(self.confirm)
-        self.ui.pushButton_3.clicked.connect(self.message)
-        self.ui.pushButton_4.clicked.connect(self.error_message)
+
+        # Обработчики
+        self.ui.pushButton.clicked.connect(self.modal)
+        self.ui.pushButton_2.clicked.connect(self.qt_modal)
+
+
+    def qt_modal(self):
+        window = Modal_Window(self)
+        window.show()
+
+    def modal(self):
+        window = QtWidgets.QWidget(self, QtCore.Qt.Window)
+        window.setWindowTitle('title')
+        window.setWindowModality(2)
+        window.resize(500, 500)
+        window.show()
+
+
+
+class Modal_Window(QtWidgets.QWidget):
+    def __init__(self, parent=GUI):
+        super().__init__(parent, QtCore.Qt.Window)
+        self.modal = Ui_Form()
+        self.modal.setupUi(self)
+        # Блокируем родительское окно
+        self.setWindowModality(2)
+        # 0 - не использовать модальное окно
+        # 1 - использовать модальное окно тольк для предидущих окон
+        # 2 - использовать моадльное окно для всех окно
+
         
-    def notification(self):
-        # Вызво оповещения
-        QtWidgets.QMessageBox.about(self, 'title', 'message')
-        QtWidgets.QMessageBox.warning(self, 'title', 'message')
-        QtWidgets.QMessageBox.information(self, 'title', 'message')
-        QtWidgets.QMessageBox.critical(self, 'title', 'message')
 
-    def confirm(self):
-        result = QtWidgets.QMessageBox.question(
-            self,
-            'Header',
-            'Discription...',
-            QtWidgets.QMessageBox.Yes,
-            QtWidgets.QMessageBox.No
-        )
 
-        if result == QtWidgets.QMessageBox.Yes:
-            print('Yes')
-        else:
-            print('No')
-
-        # QtWidgets.QMessageBox.Ok
-        # QtWidgets.QMessageBox.Open
-        # QtWidgets.QMessageBox.Save
-        # QtWidgets.QMessageBox.Cancel
-        # QtWidgets.QMessageBox.Close
-        # QtWidgets.QMessageBox.Yes
-        # QtWidgets.QMessageBox.No
-        # QtWidgets.QMessageBox.Abort
-        # QtWidgets.QMessageBox.Retry
-        # QtWidgets.QMessageBox.Ignore
-
-    def message(self):
-        # Вывод подробного сообщения с доп.инфо.
-        msg = QtWidgets.QMessageBox()
-        msg.setIcon(QtWidgets.QMessageBox.Critical)
-        msg.setWindowTitle('title')
-        msg.setInformativeText('desroption')
-        msg.setDetailedText('detailed...')
-        msg.setStandardButtons(QtWidgets.QMessageBox.Ok | QtWidgets.QMessageBox.Cancel)
-        item = msg.exec_()
-
-        if item == QtWidgets.QMessageBox.Ok:
-            print('Ok')
-        elif item == QtWidgets.QMessageBox.Cancel:
-            print('Cancel')
-
-    def error_message(self):
-        # Ошибка которая дает возможность отключить ее повторный вывод
-        error = QtWidgets.QErrorMessage(self)
-        error.setWindowTitle('Title')
-        error.showMessage('error_message')
 
 
 if __name__ == "__main__":
--- ui.py
+++ ui.py
@@ -14,21 +14,15 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(514, 141)
+        MainWindow.resize(351, 115)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
-        self.pushButton_2 = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton_2.setGeometry(QtCore.QRect(200, 20, 291, 51))
-        self.pushButton_2.setObjectName("pushButton_2")
         self.pushButton = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton.setGeometry(QtCore.QRect(20, 20, 151, 51))
+        self.pushButton.setGeometry(QtCore.QRect(10, 40, 161, 61))
         self.pushButton.setObjectName("pushButton")
-        self.pushButton_3 = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton_3.setGeometry(QtCore.QRect(20, 80, 291, 51))
-        self.pushButton_3.setObjectName("pushButton_3")
-        self.pushButton_4 = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton_4.setGeometry(QtCore.QRect(340, 80, 151, 51))
-        self.pushButton_4.setObjectName("pushButton_4")
+        self.pushButton_2 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_2.setGeometry(QtCore.QRect(180, 40, 161, 61))
+        self.pushButton_2.setObjectName("pushButton_2")
         MainWindow.setCentralWidget(self.centralwidget)
 
         self.retranslateUi(MainWindow)
@@ -37,7 +31,5 @@ class Ui_MainWindow(object):
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
-        self.pushButton_2.setText(_translate("MainWindow", "Оповещение с подтверждением пользователя"))
-        self.pushButton.setText(_translate("MainWindow", "Вывести оповещение"))
-        self.pushButton_3.setText(_translate("MainWindow", "Вывести подробное оповещение"))
-        self.pushButton_4.setText(_translate("MainWindow", "Вывести ошибку"))
+        self.pushButton.setText(_translate("MainWindow", "Открыть окно"))
+        self.pushButton_2.setText(_translate("MainWindow", "Открыть окно QT"))
