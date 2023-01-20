889adedacd4d6922ba6787d07b956ebda5cabf0b
ARTUR <rogozinskiyartur@gmail,com>
2023-01-20 15:14:14 +0300

    Уведомления Урок 8 QThread

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
 import sys
+import time
 
 from PyQt5 import QtCore, QtGui, QtWidgets
 
 from ui import *
         
+
+class Handler(QtCore.QThread):
+    mysignal = QtCore.pyqtSignal(list)
+
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+    def run(self):
+        arrage =list(range(0, 10))
+        self.mysignal.emit(arrage)
+
 
 class GUI(QtWidgets.QMainWindow):
     def __init__(self, parent=None):
@@ -11,11 +23,21 @@ class GUI(QtWidgets.QMainWindow):
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
 
-        self.ui.actionOpen_file.triggered.connect(self.test)
+        self.ui.pushButton.clicked.connect(self.start_handler)
 
-    def test(self):
-        self.ui.statusbar.showMessage('Work')
 
+        # Когда в Handler выполняется emit(), то код ниже
+        # ловит сигнал и передает его в "self.add_item"
+        self.mythread = Handler()
+        self.mythread.mysignal.connect(self.add_item)
+
+
+    def start_handler(self):
+        self.mythread.start()
+
+    def add_item(self, value):
+        for i in value:
+            self.ui.plainTextEdit.appendPlainText(str(i))
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
